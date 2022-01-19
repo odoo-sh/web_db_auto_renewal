7fb991fec14219fd5e6b9ea10e6ce636b3590f80
Karthik, Sodexis <karthik@sodexis.com>
2022-01-19 19:21:12 +0530

    [ID: 22322] Auto subscription base64 encode - added cisco_type7 encode

--- web_db_auto_renewal/__manifest__.py
+++ web_db_auto_renewal/__manifest__.py
@@ -7,7 +7,7 @@
 
         This module allow will automatically renews the expired
          database when any user log in into that DB.""",
-    'version': "15.0.1.0.0",
+    'version': "15.0.2.0.0",
     'category': 'Uncategorized',
     'website': "http://sodexis.com/",
     'author': "Sodexis",
--- web_db_auto_renewal/controllers/main.py
+++ web_db_auto_renewal/controllers/main.py
@@ -1,6 +1,8 @@
 # Copyright 2020-22 Sodexis
 # License OEEL-1 (See LICENSE file for full copyright and licensing details).
 
+from passlib.hash import cisco_type7 as t7
+
 from odoo.addons.web.controllers.main import Home
 from odoo.http import request
 from odoo.tools import config, DEFAULT_SERVER_DATETIME_FORMAT
@@ -14,7 +16,10 @@ class HomeExtended(Home):
         set_param = request.env['ir.config_parameter'].sudo().set_param
         expiry_date = fields.Datetime.to_datetime(get_param('database.expiration_date'))
         today = fields.Datetime.now()
-        partner_code = config.get('partner_code', False)
+        partner_code_hash = config.get('partner_code', False)
+        partner_code = False
+        if partner_code_hash:
+            partner_code = t7.decode(partner_code_hash)
         if partner_code and (expiry_date < today):
             set_param('database.enterprise_code', partner_code)
             request.env['publisher_warranty.contract'].update_notification()
